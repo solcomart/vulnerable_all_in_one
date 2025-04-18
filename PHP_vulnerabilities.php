fc748c4d3c2bc01cd207cd7afbe42743c8388e85
Mikhail Solovyev <solcomart@gmail.com>
2025-04-18 21:19:46 +0300

    Комментарии и несколько дополнительных примеров

--- PHP_vulnerabilities.php
+++ PHP_vulnerabilities.php
@@ -14,12 +14,40 @@ $result = mysqli_query($conn, $query);
 
 
 // [SQL Injection][Hard]
-function getUserById($id) {
-    global $conn;
-    $query = "SELECT * FROM users WHERE id = '" . addslashes($id) . "'";
-    return mysqli_query($conn, $query);
+function getUserById($id)
+{
+	global $conn;
+	$query = "SELECT * FROM users WHERE id = '" . addslashes($id) . "'";
+	return mysqli_query($conn, $query);
 }
+
 getUserById($_GET['id']);
+
+// [SQL Injection][Mikhail]
+// это уязвимость
+// строку, прочитанную из Redis, нужно санировать перед запросом в БД
+$redis = new Redis();
+$redis->connect('127.0.0.1', 6379);
+
+$name = $redis->get('name');
+$email = $redis->get('email');
+$result = mysqli_query($conn, "INSERT INTO users (id, name, email)
+VALUES ('$id', '$name', '$email');");
+
+
+// [SQL Injection][Mikhail][Safe]
+// на такой код анализатор реагировать по идее не должен
+// если реагирует, это можно считать ложным срабатыванием
+// из строки удаляются все управляющие символы
+function getUserByIdCustom($id)
+{
+	global $conn;
+	$id = preg_replace('/[^a-zA-Z0-9]/', '', $id);
+	$query = "SELECT * FROM users WHERE id = '" . addslashes($id) . "'";
+	return mysqli_query($conn, $query);
+}
+
+getUserByIdCustom($_GET['id']);
 
 
 // [XSS][Easy]
@@ -36,6 +64,35 @@ $name = $_POST['name'];
 echo "<script>var user = '$name';</script>";
 
 
+// [XSS][Mikhail]
+function render($data)
+{
+	return $data;
+}
+
+echo render($_GET['msg']);
+
+
+// [XSS][Mikhail]
+// строку, прочитанную из Redis, тоже надо санировать перед выводом
+$redis = new Redis();
+$redis->connect('127.0.0.1', 6379);
+
+$value = $redis->get('123');
+echo $value;
+
+
+// [XSS][Mikhail]
+// addslashes это не средство от XSS
+function escapeJs($input)
+{
+	return addslashes($input);
+}
+
+$js = $_GET['js'];
+echo "<script>var msg = '" . escapeJs($js) . "';</script>";
+
+
 // [RCE][Easy]
 eval($_GET['cmd']);
 
@@ -47,9 +104,11 @@ call_user_func($func, $args);
 
 
 // [RCE][Hard]
+// просто для информации: у нас на многих серверах отключены такие команды как system, exec, shell_exec, eval
+// проверять такой код всё равно надо
 $cmd = $_POST['cmd'];
 if (preg_match('/^[a-z]+$/', $cmd)) {
-    system($cmd);
+	system($cmd);
 }
 
 
@@ -73,8 +132,8 @@ move_uploaded_file($_FILES['file']['tmp_name'], "uploads/" . $_FILES['file']['na
 
 // [File Upload][Medium]
 $ext = pathinfo($_FILES['file']['name'], PATHINFO_EXTENSION);
-if (in_array($ext, ['jpg','png','gif'])) {
-    move_uploaded_file($_FILES['file']['tmp_name'], "uploads/" . $_FILES['file']['name']);
+if (in_array($ext, ['jpg', 'png', 'gif'])) {
+	move_uploaded_file($_FILES['file']['tmp_name'], "uploads/" . $_FILES['file']['name']);
 }
 
 
@@ -85,7 +144,7 @@ header("Location: " . $_GET['redirect']);
 // [Open Redirect][Medium]
 $url = $_GET['url'];
 if (strpos($url, "http") === 0) {
-    header("Location: $url");
+	header("Location: $url");
 }
 
 
@@ -127,6 +186,16 @@ $response = curl_init("http://" . $host);
 curl_exec($response);
 
 
+// [SSRF][Mikhail][Safe]
+// код безопасный - не должно быть срабатывания
+$host = $_POST['host'];
+if (!in_array($host, ['tilda.by', 'tilda.cc'])) {
+	die('Wrong host');
+}
+$response = curl_init("http://" . $host);
+curl_exec($response);
+
+
 // [Insecure Crypto][Easy]
 echo md5($_POST['password']);
 
@@ -138,15 +207,15 @@ $encrypted = openssl_encrypt($_POST['data'], 'aes128', $key);
 
 // [Mass Assignment][Easy]
 foreach ($_POST as $k => $v) {
-    $user->$k = $v;
+	$user->$k = $v;
 }
 
 
 // [Mass Assignment][Medium]
 $allowed = ['name', 'email'];
 foreach ($_POST as $k => $v) {
-    if (in_array($k, $allowed)) {
-        $user->$k = $v;
-    }
+	if (in_array($k, $allowed)) {
+		$user->$k = $v;
+	}
 }
 ?>
